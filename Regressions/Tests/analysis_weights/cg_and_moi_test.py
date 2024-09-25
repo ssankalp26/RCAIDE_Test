961d45298be7bc1f32f4786ba8a92dcb78352f3c
Matthew Clarke <mclarke2@stanford.edu>
2024-09-25 09:08:57 -0500

    adding regression test

---
+++ Regressions/Tests/analysis_weights/cg_and_moi_test.py
@@ -0,0 +1,36 @@
+# cg_and_moi_test.py   
+from RCAIDE.Framework.Core import Units,  Data ,  Container 
+
+from Legacy.trunk.S.Input_Output.SUAVE.load    import load as load_results
+from Legacy.trunk.S.Input_Output.SUAVE.archive import archive as save_results 
+
+import numpy as  np 
+import sys
+
+sys.path.append('../../Vehicles')
+# the analysis functions
+
+from Boeing_737             import vehicle_setup as transport_setup
+from Cessna_172             import vehicle_setup as general_aviation_setup
+
+def main(): 
+    Transport_Aircraft_Test()
+    BWB_Aircraft_Test()
+    return
+
+def General_Aviation_Test(): 
+
+    vehicle                = general_aviation_setup()	
+    weight                 = General_Aviation.compute_operating_empty_weight(vehicle)	  
+ 
+    return  
+
+def EVTOL_Aircraft_Test(): 
+    vehicle = evtol_setup()
+    weight  = Electric.compute_operating_empty_weight(vehicle)
+    
+    return 
+ 
+
+if __name__ == '__main__':
+    main()
--- Regressions/automatic_regression.py
+++ Regressions/automatic_regression.py
@@ -24,6 +24,8 @@ modules = [
     'Tests/analysis_noise/empirical_jet_noise_test.py',     
     'Tests/analysis_noise/noise_hemisphere_test.py', 
     'Tests/analysis_stability/vlm_pertubation_test.py', 
+    'Tests/analysis_weights/operating_empty_weight_test.py',
+    'Tests/analysis_weights/cg_and_moi_test.py',
     'Tests/geometry_airfoils/airfoil_import_test.py', 
     'Tests/geometry_airfoils/airfoil_interpolation_test.py',    
     'Tests/mission_segments/segment_test.py',     
