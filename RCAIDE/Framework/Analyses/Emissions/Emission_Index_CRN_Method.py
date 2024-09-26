4fbc36303d479c806f43511a8e50cfe063bc1da9
MGuidotti <matteog2@illinois.edu>
2024-09-26 13:06:53 -0500

    Update Emission_Index_CRN_Method.py

--- RCAIDE/Framework/Analyses/Emissions/Emission_Index_CRN_Method.py
+++ RCAIDE/Framework/Analyses/Emissions/Emission_Index_CRN_Method.py
@@ -38,10 +38,10 @@ class Emission_Index_CRN_Method(Emissions):
         #self.training.temperature       = np.linspace(600, 1000, 10) 
         #self.training.air_mass_flowrate = np.linspace(1E-3, 1E2, 10) 
         #self.training.fuel_to_air_ratio = np.linspace(0.01, 0.1, 10)
-        self.training.pressure          = np.linspace(10,30, 3) *1E6
-        self.training.temperature       = np.linspace(700, 900, 3) 
-        self.training.air_mass_flowrate = np.linspace(10, 60, 3) 
-        self.training.fuel_to_air_ratio = np.linspace(0.01, 0.05, 3)        
+        self.training.pressure          = np.linspace(10,30, 2) *1E6
+        self.training.temperature       = np.linspace(700, 900, 2) 
+        self.training.air_mass_flowrate = np.linspace(10, 60, 2) 
+        self.training.fuel_to_air_ratio = np.linspace(0.01, 0.05, 2)        
         
         # surrogoate models                 
         self.surrogates                  = Data() 
