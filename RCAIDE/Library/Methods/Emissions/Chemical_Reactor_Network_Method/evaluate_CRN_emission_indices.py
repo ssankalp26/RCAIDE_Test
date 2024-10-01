9dde9738e69a4121551fcbfe901ae36f94e0fb94
Matthew Clarke <mclarke2@stanford.edu>
2024-09-30 19:08:31 -0500

    Update evaluate_CRN_emission_indices.py

--- RCAIDE/Library/Methods/Emissions/Chemical_Reactor_Network_Method/evaluate_CRN_emission_indices.py
+++ RCAIDE/Library/Methods/Emissions/Chemical_Reactor_Network_Method/evaluate_CRN_emission_indices.py
@@ -72,14 +72,14 @@ def evaluate_CRN_emission_indices_no_surrogate(segment,settings,vehicle):
                                 NO_total   += np.dot(I,mdot *EI_NO_p ) 
                                 NO2_total  += np.dot(I,mdot *EI_NO2_p)
 
-    emissions                 = Data()
-    emissions.total           = Data()
-    emissions.index           = Data() 
-    emissions.total.CO2      = CO2_total  * combustor.fuel.global_warming_potential_100.NOx 
-    emissions.total.CO       = CO_total   * combustor.fuel.global_warming_potential_100.CO2
-    emissions.total.H2O      = H2O_total  * combustor.fuel.global_warming_potential_100.H2O  
-    emissions.total.NO       = NO_total   * combustor.fuel.global_warming_potential_100.SO2  
-    emissions.total.NO2      = NO2_total  * combustor.fuel.global_warming_potential_100.Soot  
+    emissions               = Data()
+    emissions.total         = Data()
+    emissions.index         = Data() 
+    emissions.total.CO2     = CO2_total  * combustor.fuel.global_warming_potential_100.NOx 
+    emissions.total.CO      = CO_total   * combustor.fuel.global_warming_potential_100.CO2
+    emissions.total.H2O     = H2O_total  * combustor.fuel.global_warming_potential_100.H2O  
+    emissions.total.NO      = NO_total   * combustor.fuel.global_warming_potential_100.SO2  
+    emissions.total.NO2     = NO2_total  * combustor.fuel.global_warming_potential_100.Soot  
     emissions.index.CO2     = EI_CO2_p
     emissions.index.CO      = EI_CO_p 
     emissions.index.H2O     = EI_H2O_p
@@ -116,14 +116,12 @@ def evaluate_CRN_emission_indices_surrogate(segment,settings,vehicle):
                                 propulsor_conditions    = segment.state.conditions.energy[fuel_line.tag][propulsor.tag] 
                                 combustor_conditions    = propulsor_conditions[combustor.tag]  
 
-                                T = combustor_conditions.inputs.stagnation_temperature
-                                P = combustor_conditions.inputs.stagnation_pressure 
+                                T    = combustor_conditions.inputs.stagnation_temperature
+                                P    = combustor_conditions.inputs.stagnation_pressure 
                                 mdot = propulsor_conditions.core_mass_flow_rate 
-                                FAR = combustor_conditions.outputs.fuel_to_air_ratio 
+                                FAR  = combustor_conditions.outputs.fuel_to_air_ratio 
                                 
-                                pts = np.hstack((T,P,mdot,FAR ))
-                                
-                                #results = evaluate_cantera(combustor,pts)
+                                pts = np.hstack((T,P,mdot,FAR )) 
 
                                 EI_CO2_p  = np.atleast_2d(surrogates.EI_CO2(pts)).T
                                 EI_CO_p   = np.atleast_2d(surrogates.EI_CO(pts)).T 
@@ -140,14 +138,15 @@ def evaluate_CRN_emission_indices_surrogate(segment,settings,vehicle):
     emissions                 = Data()
     emissions.total           = Data()
     emissions.index           = Data() 
-    emissions.total.CO2       = CO2_total  * combustor.fuel.global_warming_potential_100.CO2
-    emissions.total.H2O       = H2O_total  * combustor.fuel.global_warming_potential_100.H2O   
+    emissions.total.CO2       = CO2_total  * combustor.fuel_data.global_warming_potential_100.CO2 
+    emissions.total.H2O       = H2O_total  * combustor.fuel_data.global_warming_potential_100.H2O  
+    emissions.total.NOx       = (NO_total +NO2_total) * combustor.fuel_data.global_warming_potential_100.NOx 
     emissions.index.CO2       = EI_CO2_p
     emissions.index.CO        = EI_CO_p 
     emissions.index.H2O       = EI_H2O_p
     emissions.index.NO        = EI_NO_p 
-    emissions.index.NO2       = EI_NO2_p
-    
+    emissions.index.NO2       = EI_NO2_p 
+ 
     segment.state.conditions.emissions =  emissions
     return   
 
