d605d76c569634fe817623b87e494906a79edb08
Matthew Clarke <mclarke2@stanford.edu>
2024-10-01 20:24:35 -0500

    Update __init__.py

--- Legacy/trunk/S/__init__.py
+++ Legacy/trunk/S/__init__.py
@@ -15,9 +15,7 @@ from . import Components
 from . import Analyses
 from . import Methods
 from . import Attributes
-from . import Optimization
-from . import Input_Output
-from . import Plots
+from . import Optimization 
 
 # the vehicle class
 from .Vehicle import Vehicle
