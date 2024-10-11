1cd7f769e2ef6e4b1c81c3378effc7c81d3893b0
niranjann-14 <nn18@illinois.edu>
2024-10-10 20:12:51 -0500

    Changes

--- Regressions/Tests/analysis_noise/noise_hemisphere_test.py
+++ Regressions/Tests/analysis_noise/noise_hemisphere_test.py
@@ -35,7 +35,7 @@ def main():
     noise_data = plot_results(results,regression_plotting_flag) 
  
     # dBA Verification checj
-    dBA_true   = 70.0732251155458
+    dBA_true   = 162.6010473612175
     dBA        = noise_data.SPL_dBA[0,0,0]
     print('dBA: ' + str(dBA)) 
     diff_dBA                = np.abs(dBA  - dBA_true) 
