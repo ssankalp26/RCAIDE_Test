4d0fced51e77325b319f760d2c218112875c5234
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2024-10-14 13:07:54 -0500

    Update Geodesics.py

--- RCAIDE/Framework/Analyses/Geodesics/Geodesics.py
+++ RCAIDE/Framework/Analyses/Geodesics/Geodesics.py
@@ -13,8 +13,19 @@ from  RCAIDE.Library.Methods.Geodesics.Geodesics import Geodesic_Calculate
 #  Calculate Distance between two coordinate locations
 # ----------------------------------------------------------------------------------------------------------------------  
 def Calculate_Distance(coord1, coord2):
+    """This passes the coordinates to the distance calculation method and then returns the results in kilometers
+    
+       Inputs:
+       - Coordinates (lat, long)
+       
+       Outputs:
+       - Distance in kilometers between the two coordinates.
+       
+       Assumptions:
+       None
+
+       Source:
+       None 
+            """    
     distance = Geodesic_Calculate(coord1, coord2).kilometers
     return(distance)
-
-if __name__ == '__main__': 
-    Calculate_Distance()
