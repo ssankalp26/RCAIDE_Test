c4b5b1ca3a9bf2e2559669ff88d92e4cfeb7649e
MGuidotti <matteog2@illinois.edu>
2024-10-31 16:01:37 -0500

    Aircraft Project Emissions

--- RCAIDE/Library/Attributes/Propellants/Liquid_Natural_Gas.py
+++ RCAIDE/Library/Attributes/Propellants/Liquid_Natural_Gas.py
@@ -32,3 +32,13 @@ class Liquid_Natural_Gas(Propellant):
         self.density         = 414.2                            # kg/m^3 
         self.specific_energy = 53.6e6                           # J/kg
         self.energy_density  = 22200.0e6                        # J/m^3
+        
+        self.use_high_fidelity_kinetics_model      =  True 
+        self.fuel_surrogate_chemical_properties    = {'CH4':0.85, 'C2H6':0.1, 'C3H8':0.05}
+        self.fuel_chemical_properties              = {'CH4':0.85, 'C2H6':0.1, 'C3H8':0.05}         
+        self.air_chemical_properties               = {'O2':0.2095, 'N2':0.7809, 'AR':0.0096}
+        self.surrogate_species_list                = ['CO', 'CO2', 'H2O']
+        self.species_list                          = ['CO', 'CO2', 'H2O', 'NO', 'NO2', 'CSOLID']
+        self.surrogate_chemical_kinetics           = 'Fuel_Surrogate.yaml'
+        self.chemical_kinetics                     = 'Fuel.yaml'
+        self.oxidizer                              = 'Air.yaml'        
