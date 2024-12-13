c2217c899b35e51417c0d99a331a43270efbe27a
Matteo Guidotti <matteog2@illinois.edu>
2024-12-13 17:33:13 -0600

    Removal of legacy

--- RCAIDE/Library/Methods/Aerodynamics/Common/Helper_Functions/__init__.py
+++ RCAIDE/Library/Methods/Aerodynamics/Common/Helper_Functions/__init__.py
@@ -1,16 +1,11 @@
-# RCAIDE/Methods/Aerodynamics/Common/Helper_Functions/__init__.py
-# 
+## @defgroup Methods-Aerodynamics-Common-Fidelity_Zero-Helper_Functions Helper_Functions
+# Functions that are needed by aerodynamics methods.
+# @ingroup Methods-Aerodynamics-Common-Fidelity_Zero
 
-""" RCAIDE Package Setup
-"""
-
-# ----------------------------------------------------------------------------------------------------------------------
-#  IMPORT
-# ---------------------------------------------------------------------------------------------------------------------- 
-
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.windmilling_drag                  import windmilling_drag
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.asymmetry_drag                    import asymmetry_drag
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.estimate_2ndseg_lift_drag_ratio   import estimate_2ndseg_lift_drag_ratio
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.compressible_turbulent_flat_plate import compressible_turbulent_flat_plate
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.compressible_mixed_flat_plate     import compressible_mixed_flat_plate
-from Legacy.trunk.S.Methods.Aerodynamics.Common.Fidelity_Zero.Helper_Functions.wave_drag_lift                    import wave_drag_lift
+from .compressible_mixed_flat_plate import compressible_mixed_flat_plate
+from .windmilling_drag import windmilling_drag
+from .asymmetry_drag import asymmetry_drag
+from .estimate_2ndseg_lift_drag_ratio import estimate_2ndseg_lift_drag_ratio
+from .compressible_turbulent_flat_plate import compressible_turbulent_flat_plate
+from .compressible_mixed_flat_plate import compressible_mixed_flat_plate
+from .wave_drag_lift import wave_drag_lift
