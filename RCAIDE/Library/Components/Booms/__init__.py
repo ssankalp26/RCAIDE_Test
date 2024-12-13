27484c08577ccc41444108441a22c24a9df5bea9
ARmolloy <167649645+ARmolloy@users.noreply.github.com>
2024-12-13 14:08:28 -0600

    Update __init__.py

--- RCAIDE/Library/Components/Booms/__init__.py
+++ RCAIDE/Library/Components/Booms/__init__.py
@@ -1,12 +1,22 @@
-## @defgroup Library-Components-Booms Booms
 # RCAIDE/Library/Components/Booms/__init__.py
-# 
+# RCAIDE/Library/Components/Booms/__init__.py
+"""
+=============================================
+Booms (:mod:`RCAIDE.Library.Components.Booms`)
+=============================================
 
-"""RCAIDE Package Setup
+.. currentmodule:: RCAIDE.Library.Components.Booms
+
+This module contains the boom class for RCAIDE.
+
+Classes
+-------
+.. autosummary::
+   :toctree: generated/
+   
+   Boom    -- Base class for aircraft boom components
 """
 
-# ----------------------------------------------------------------------------------------------------------------------
-#  IMPORT
-# ----------------------------------------------------------------------------------------------------------------------
+from .Boom import Boom
 
-from .Boom import Boom 
+from .Boom import Boom
